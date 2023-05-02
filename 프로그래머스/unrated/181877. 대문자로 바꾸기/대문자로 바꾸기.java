25abf1e4279cd1deaacc5d9d4a22df62542c4130
HakCoding <qudgkr1230@naver.com>
2023-05-02 14:01:28 +0900

    [unrated] Title: 대문자로 바꾸기, Time: 0.02 ms, Memory: 70.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181877. 대문자로 바꾸기/대문자로 바꾸기.java
@@ -0,0 +1,7 @@
+class Solution {
+    public String solution(String myString) {
+        String answer = "";
+        answer = myString.toUpperCase(); // 모든 글자를 대문자로 바꾼다
+        return answer;
+    }
+}

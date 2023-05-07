e421eb66786a138e01256a73ca44c79819a6312b
HakCoding <qudgkr1230@naver.com>
2023-05-07 12:55:02 +0900

    [unrated] Title: 홀수 vs 짝수, Time: 0.02 ms, Memory: 75.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181887. 홀수 vs 짝수/홀수 vs 짝수.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(int[] num_list) {
+        int answer = 0;
+        int sum1 = 0, sum2 = 0;
+        for (int i = 0; i < num_list.length; i++) {
+		    if(i % 2 != 0) {
+		    	sum1 = sum1 + num_list[i];
+		    }
+		    else {
+		    	sum2 = sum2 + num_list[i];
+		    }
+		} 
+		answer = sum1;
+		if(answer < sum2) answer = sum2;
+        return answer;
+    }
+}

ad0bd73407aa8a4baa5c9a35e507a1f90f66d5c2
HakCoding <qudgkr1230@naver.com>
2023-05-11 10:16:41 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.02 ms, Memory: 73.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120819. 아이스 아메리카노/아이스 아메리카노.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int[] solution(int money) {
+        int[] answer = new int[2];
+        
+        answer[0] = money / 5500; // 아메리카노 잔 수
+        answer[1] = money % 5500; // 거스름돈
+        return answer;
+    }
+}

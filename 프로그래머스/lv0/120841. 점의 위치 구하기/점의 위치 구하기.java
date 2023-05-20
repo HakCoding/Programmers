5ea71d9ebc0a9980f06ee3a981fb24d9cf363d3e
HakCoding <qudgkr1230@naver.com>
2023-05-20 13:21:25 +0900

    [level 0] Title: 점의 위치 구하기, Time: 0.02 ms, Memory: 76.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120841. 점의 위치 구하기/점의 위치 구하기.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(int[] dot) {
+        int answer = 0;
+        if(dot[0]>0 && dot[1]>0) {         
+            answer=1;  //1사분면
+        }
+        else if(dot[0]<0 && dot[1]>0) {      
+            answer=2; //2사분면  
+        }
+        else if(dot[0]<0 && dot[1]<0) {       
+            answer=3; //3사분면 
+        }
+        else if(dot[0]>0 && dot[1]<0) {   
+            answer=4; //4사분면
+        }
+        return answer;
+    }
+}

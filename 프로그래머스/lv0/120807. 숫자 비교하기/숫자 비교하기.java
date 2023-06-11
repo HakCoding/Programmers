06bea8a9174aa8463a8aefe1184d098a01ac2f99
HakCoding <qudgkr1230@naver.com>
2023-06-11 20:25:06 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.02 ms, Memory: 78 MB -BaekjoonHub

--- 프로그래머스/lv0/120807. 숫자 비교하기/숫자 비교하기.java
+++ 프로그래머스/lv0/120807. 숫자 비교하기/숫자 비교하기.java
@@ -2,6 +2,6 @@ class Solution {
     public int solution(int num1, int num2) {
         int answer = -1;
         if(num1 == num2) answer = 1; // 숫자가 같은지 비교
-        return answer;
+        return answer; 
     }
 }

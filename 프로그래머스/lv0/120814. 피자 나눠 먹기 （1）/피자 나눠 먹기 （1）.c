60e96ae60f2f27f5986fa108d3d2e9995b7bfeab
HakCoding <qudgkr1230@naver.com>
2023-06-15 21:31:35 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.01 ms, Memory: 4.19 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int n) {
+    int answer = 1;
+    if(n / 7 != 0){ 
+        answer = n/7; // 남은 피자를 한조각씩 먹는다
+        if(n % 7 !=0){
+            answer += 1; // 혹시 못먹은 사람이 있으면 1판 추가
+        }
+    }
+    return answer;
+}
